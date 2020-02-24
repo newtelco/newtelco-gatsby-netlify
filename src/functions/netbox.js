ba8f5389e7fb9ffef902f9e9b0a1fdfa6bfa0696
ndo@ndo1 <yo@ni.co.de>
2020-02-24 18:09:57 +0100

    update: netlify function

--- src/functions/netbox.js
+++ src/functions/netbox.js
@@ -4,7 +4,7 @@ const { NETBOX_KEY } = process.env
 const API_ENDPOINT = 'https://racks.newtelco.de/api/dcim/sites/'
 
 exports.handler = async (event, context) => {
-  const resp = fetch(API_ENDPOINT, {
+  return fetch(API_ENDPOINT, {
     headers: {
       Accept: 'application/json',
       Authorization: `Token ${NETBOX_KEY}`,
@@ -16,8 +16,4 @@ exports.handler = async (event, context) => {
       body: data,
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }))
-
-  console.log(resp)
-
-  return resp
 }
