4e4dbf4998e30ac26f7b11275491f5c206e10861
ndo@ndo1 <yo@ndo.dev>
2020-03-11 11:23:07 +0100

    add: serviceWorker Reload script

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,18 @@
-import 'lazysizes';
+import 'lazysizes'
 
-require('prismjs/themes/prism-tomorrow.css');
+require('prismjs/themes/prism-tomorrow.css')
 
-import CustomLayout from './wrapPageElement';
+import CustomLayout from './wrapPageElement'
 
-export const wrapPageElement = CustomLayout;
+export const wrapPageElement = CustomLayout
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    'This application has been updated. ' +
+      'Reload to display the latest version?'
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
