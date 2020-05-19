93febe48944fc5ea2ee0f8d620005bf6c5bc77c4
ndo@ndo3 <yo@ndo.dev>
2020-05-20 01:19:12 +0200

    fix: add missing stuff!

---
+++ src/components/SectionTestimonials/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Carousel from '../Carousel'
+import * as S from './styled'
+
+const SectionTestimonial = props => {
+  return (
+    <S.Wrapper>
+      <S.Content>
+        <Carousel />
+      </S.Content>
+    </S.Wrapper>
+  )
+}
+
+export default SectionTestimonial
---
+++ src/components/SectionTestimonials/styled.js
@@ -0,0 +1,70 @@
+import styled from 'styled-components'
+import media from 'styled-media-query'
+
+export const Wrapper = styled.div`
+  display: flex;
+  height: 300px;
+  flex-direction: column;
+  margin: 50px 0px 0px 0px;
+`
+export const SectionSvg = styled.div`
+  position: absolute;
+`
+
+export const Content = styled.section`
+  display: flex;
+  flex: 1;
+  width: 100%;
+  justify-content: space-around;
+  ${media.lessThan('medium')`
+    flex-direction: row;
+  `}
+`
+
+export const Container = styled.span`
+  &:nth-of-type(1) {
+    transition-delay: 150ms;
+  }
+  &:nth-of-type(2) {
+    transition-delay: 250ms;
+  }
+  &:nth-of-type(3) {
+    transition-delay: 350ms;
+  }
+`
+
+export const Header = styled.h3`
+  font-size: 3.5rem;
+  font-weight: 600;
+  margin: 100px auto 50px auto;
+
+  &:before {
+    display: inline-block;
+    content: '';
+    border-top: 0.3rem solid var(--primary-color);
+    width: 8rem;
+    margin: 0 1rem;
+    transform: translateY(-1rem);
+  }
+
+  &:after {
+    display: inline-block;
+    content: '';
+    border-top: 0.3rem solid var(--primary-color);
+    width: 8rem;
+    margin: 0 1rem;
+    transform: translateY(-1rem);
+  }
+  ${media.lessThan('medium')`
+    &::before {
+      border-top: none;
+      display: none;
+      content: none;
+    }
+    &::after {
+      border-top: none;
+      display: none;
+      content: none;
+    }
+  `}
+`
