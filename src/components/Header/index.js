282bc414e50ac824c7d119b06fa621a442a5aa13
ndo@ndo3 <yo@ndo.dev>
2020-05-19 21:50:39 +0200

    rm: shynet completely

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -31,7 +31,7 @@ const Header = () => {
         <ButtonMenu handleClick={handleToggleMenu} isActive={toggleMenu} />
         <OverlayMenu toggleMenu={handleToggleMenu} isActive={toggleMenu} />
       </S.Container>
-      <noscript><img src='https://analytics.newtelco.dev/ingress/7b6297e9-5457-450d-b09e-227f09babeea/pixel.gif' /></noscript>
+      {/* <noscript><img src='https://analytics.newtelco.dev/ingress/7b6297e9-5457-450d-b09e-227f09babeea/pixel.gif' /></noscript> */}
       {/* <script src='https://analytics.newtelco.dev/ingress/7b6297e9-5457-450d-b09e-227f09babeea/script.js'></script> */}
     </S.HeaderWrapper>
   )
